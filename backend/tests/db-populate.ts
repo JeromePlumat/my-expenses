99550b7d568fe82e01a451392a5215ecf460e39e
Jerome Plumat <jerome.plumat@vinci.be>
2026-09-22 16:49:19 +0200

    refactors files

--- backend/tests/db-populate.ts
+++ backend/tests/db-populate.ts
@@ -1,4 +1,4 @@
-import { db } from './src/prisma/db.ts';
+import { db } from '../src/prisma/db.ts';
 
 async function main() {
   const expense = await db.orm.public.Expense.create({
--- backend/tests/test-db-read.ts
+++ backend/tests/test-db-read.ts
@@ -1,4 +1,4 @@
-import { db } from './src/prisma/db.ts';
+import { db } from '../src/prisma/db.ts';
 
 async function main() {
   const expenses = await db.orm.public.Expense.all();
