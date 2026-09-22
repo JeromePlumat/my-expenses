a076075ae7f04112c69d90f6e0671e61d8e10f4e
Jerome Plumat <jerome.plumat@vinci.be>
2026-09-22 16:57:48 +0200

    adds hook form

--- frontend/src/components/ExpenseAdd.tsx
+++ frontend/src/components/ExpenseAdd.tsx
@@ -1,3 +1,4 @@
+import { useForm } from "react-hook-form";
 import type { NewExpense } from "../types/Expense";
 
 interface ExpenseAddProps {
@@ -5,29 +6,32 @@ interface ExpenseAddProps {
 }
 
 function ExpenseAdd({ expenseAdd }: ExpenseAddProps) {
-  const handleSubmit = (e: any) => {
-    e.preventDefault();
-    const description = (e.target as HTMLFormElement).elements.namedItem("description") as HTMLInputElement;
-    const payer = (e.target as HTMLFormElement).elements.namedItem("payer") as HTMLInputElement;
-    const amount = (e.target as HTMLFormElement).elements.namedItem("amount") as HTMLInputElement;
-    const date = (e.target as HTMLFormElement).elements.namedItem("date") as HTMLInputElement;
-    const newExpense: NewExpense = {
-        description: description.value,
-        payer: payer.value,
-        amount: parseFloat(amount.value),
-        date: date.value,
-    };
-    expenseAdd(newExpense);
+  const onSubmit = async (e: NewExpense) => {
+  //   const description = (e.target as HTMLFormElement).elements.namedItem("description") as HTMLInputElement;
+  //   const payer = (e.target as HTMLFormElement).elements.namedItem("payer") as HTMLInputElement;
+  //   const amount = (e.target as HTMLFormElement).elements.namedItem("amount") as HTMLInputElement;
+  //   const date = (e.target as HTMLFormElement).elements.namedItem("date") as HTMLInputElement;
+  //   const newExpense: NewExpense = {
+  //       description: description.value,
+  //       payer: payer.value,
+  //       amount: parseFloat(amount.value),
+  //       date: date.value,
+  //   };
+    await expenseAdd(e);
+    reset(); // clear the form
   };
+
+  const { register, handleSubmit, reset } = useForm<NewExpense>();
+
 
   return <div>
     <h2>Add expense</h2>
-    <form onSubmit={handleSubmit}>
-        <input type="text" name="description" placeholder="Description" />
-        <input type="text" name="payer" placeholder="Payer" />
-        <input type="number" name="amount" placeholder="Amount" />
-        <input type="date" name="date" placeholder="Date" />
-        <button type="submit" className="btn btn-primary">Add</button>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("description")} placeholder="Description" />
+      <input type="text" {...register("payer")} placeholder="Payer" />
+      <input type="number" {...register("amount")} placeholder="Amount" />
+      <input type="date" {...register("date")} placeholder="Date" />
+      <button type="submit" className="btn btn-primary">Add</button>
     </form>
   </div>;
 }
